b9ff00250bf5c19425ea623b4db5d1927739f93a
Dialest <dialest@hotmail.com>
2019-07-12 00:41:34 -0700

    Add react router & App column layout &  bkgrd imgs

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Game from "./pages/Game"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='layout'>
+      <div className='sides'></div>
+      <div className="sides left-border"></div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Game} />
+          </Switch>
+        </div>
+      </Router>
+      <div className="sides right-border"></div>
+      <div className='sides'></div>  
     </div>
   );
 }
+
+// Add additional pages to the Switch as necessary
+// The column layout is added here as it will not be impacted by any other page or component
 
 export default App;
---
+++ client/src/pages/Game.js
@@ -0,0 +1,15 @@
+import React, { Component } from  "react";
+// import Layout from "../components/Layout";
+
+// The layout will likely end up beign defined on this page
+
+class Game extends Component {
+    render() {
+        return (
+            // <Layout></Layout>
+            <p>The game goes here.</p>
+        );
+    }
+};
+
+export default Game;
