2570c2fc6b774ed56c2bf3473f230ab1c51f8168
Dialest <dialest@hotmail.com>
2019-07-15 21:59:06 -0700

    Added file structure for gameplay elements

--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -2,10 +2,10 @@ import React, { Component } from  "react";
 // import Layout from "../components/Layout";
 
 // The layout will likely end up beign defined on this page
+//  The props are: rowLength, tileSize
 
 class Game extends Component {
     render() {
-        // this.butts();
         return (
             // <Layout></Layout>
             <div>
